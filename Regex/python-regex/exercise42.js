44ef9e03b03981494106b45a90d1f05599de6d58
Vivek Kurhe <vivek.kurhe@launchventures.co>
2022-12-08 21:13:13 +0530

    python regex exercise42

--- Regex/python-regex/exercise42.js
+++ Regex/python-regex/exercise42.js
@@ -1,3 +1,11 @@
 /**
  * Write a Python program to find urls in a string.
  */
+
+text =
+  '<p>Contents :</p><a href="https://w3resource.com">Python Examples</a><a href="http://github.com">Even More Examples</a>';
+
+let re = /http[s]?:\/\/[a-z0-9]+\.\w{1,3}/g;
+
+let urls = text.match(re);
+console.log(urls);
