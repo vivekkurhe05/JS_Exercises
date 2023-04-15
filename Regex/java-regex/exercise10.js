585930e2feb831b3e6802d8bcaf92fe075fa6c4a
Vivek Kurhe <vivek.kurhe@ennoventure.com>
2023-04-15 12:32:51 +0530

    practice

--- Regex/java-regex/exercise10.js
+++ Regex/java-regex/exercise10.js
@@ -4,6 +4,18 @@
 
 // my solution
 function validate(str) {
+  const re = /\b0+(?=\d)/g;
+  return str.replace(re, "");
+}
+
+console.log(validate("216.08.094.196")); // 216.8.94.196
+console.log(validate("08.008.0008.00008")); // 8.8.8.8
+console.log(validate("01.102.103.104")); // 1.102.103.104
+
+//or
+
+// my solution
+function validate(str) {
   let array_of_substrings = str.split(".");
   return array_of_substrings
     .map(function (el) {
--- Regex/python-regex/exercise19.js
+++ Regex/python-regex/exercise19.js
@@ -8,3 +8,26 @@ function text_match(str){
     return str.match(/(fox|dog|horse)/g).join(",")
 }
 console.log(text_match("The quick brown fox jumps over the lazy dog."))
+
+// or
+
+for(let i=0; i<arr.length; i++) {
+    if(text.search(new RegExp(arr[i], 'g')) != -1) {
+        console.log(`Searching for ${arr[i]} in ${text} => Matched`)
+    } else {
+        console.log(`Searching for ${arr[i]} in ${text} => Not Matched`)
+    }
+}
+
+// or
+
+let arr = ["fox", "dog", "horse"];
+let text = "The quick brown fox jumps over the lazy dog.";
+
+arr.find(function(el) {
+    if(text.includes(el)) {
+        console.log(`Searching for ${el} in ${text} => Matched`)
+    }else {
+        console.log(`Searching for ${el} in ${text} => Not Matched`)   
+    }
+})
