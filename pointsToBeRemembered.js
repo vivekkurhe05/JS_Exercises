1e6887db726cb751e284519b9e96c2b4783032aa
Vivek Kurhe <vivek.kurhe@ennoventure.com>
2023-05-13 22:31:31 +0530

    pointsToBeRemembered

--- pointsToBeRemembered.js
+++ pointsToBeRemembered.js
@@ -9,3 +9,13 @@
 // - An arrow function cannot be used as a function constructor. If you use the new keyword to create an object from an arrow function, you will get an error.
 
 // 5. The problem with the constructor function is that when you create multiple instances of the constructor function, the method is duplicated in every instance which is not memory efficient. To resolve this, you can use the prototype so that all instances can share the same method.
+
+// 6. Difference between JSON and JS object
+// - JSON all keys must be quoted, Object literals it is not necessary
+// - JSON has double quotes while javascript can use single or double quotes
+// - JavaScript can include functions which is not available in JSON
+
+// 7. Different datatypes in JSON
+// String, Number, Boolean, Object, Array of objects, Null
+
+// 8. The difference between at() and charAt() method is when specifying at(-1), it returns the last character whereas charAt(-1) returns empty string 
