edd70e8cb80065531983d07e6b8e628adef21f77
Vivek_Ennotech <vivek_ennotech@Administrators-MacBook-Pro-2.local>
2024-09-23 11:34:01 +0530

    object_methods and objectPrograms files changes

---
+++ objectPrograms.js
@@ -0,0 +1,5 @@
+/**
+ * python-dict - exercise 17
+ * 
+ * Write a Python program to remove duplicates from the dictionary.
+ */
--- object_methods.js
+++ object_methods.js
@@ -1,9 +1,13 @@
-// 1. Object.is() - It is similar to the === operator but with two differences
-// 2. Object.assign()
+// 1. Object.is(val1, val2) - It is similar to the === operator but with two differences
+// 2. Object.assign(target, source1, source2,..)
 // 3. Object.create() - It is used inside factory function
-// 4. Object.entries() - convert object into array key value pair
-// 5. Object.fromEntries() - convert array key value pair into object
-// 6. Object.freeze()
+// 4. Object.entries(object) - convert object into array key value pair
+// 5. Object.fromEntries(object) - convert array key value pair into object
+// 6. Object.freeze(object) - it prevents any changes to an object
 // 7. Object.hasOwnProperty(prop)
-// 8. Object.keys()
-// 9. Object.values()
+// 8. Object.keys(object)
+// 9. Object.values(object)
+// 10. Object.seal(object) - it prevents addition or deletion of new properties
+// 11. Object.isSealed(object) - returns true if an object is sealed.
+// 12. Object.getOwnPropertyNames(object) - returns an array with the properties of an object
+// 13. Object.preventExtensions(object) - this method prevents adding properties to an object
